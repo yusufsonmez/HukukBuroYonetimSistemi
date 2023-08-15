40fb072004d530f89b13f194aa749700db2571cc
yusufsonmez <yusufsonmez1995@gmail.com>
2023-08-16 00:04:48 +0300

    Mahkeme table nullable yapisi

---
+++ HukukBuroYonetimSistemi/Migrations/20230815210055_MahkemeTableSetNullable.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HukukBuroYonetimSistemi.Migrations
+{
+    /// <inheritdoc />
+    public partial class MahkemeTableSetNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DurusmaTarihi",
+                table: "Mahkemeler");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "YargiTuru",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "YargiBirimi",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TebligTuru",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TebligTarihi",
+                table: "Mahkemeler",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Mahkeme",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IsTuru",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "HedefSureSonGun",
+                table: "Mahkemeler",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Gorev",
+                table: "Mahkemeler",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GelmeTarihi",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GelenEvrakTuru",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DosyaNo",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Diger",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "YargiTuru",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "YargiBirimi",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TebligTuru",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TebligTarihi",
+                table: "Mahkemeler",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Mahkeme",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IsTuru",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "HedefSureSonGun",
+                table: "Mahkemeler",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Gorev",
+                table: "Mahkemeler",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GelmeTarihi",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GelenEvrakTuru",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DosyaNo",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Diger",
+                table: "Mahkemeler",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DurusmaTarihi",
+                table: "Mahkemeler",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
--- HukukBuroYonetimSistemi/Models/Domain/Mahkemeler.cs
+++ HukukBuroYonetimSistemi/Models/Domain/Mahkemeler.cs
@@ -6,24 +6,24 @@ namespace HukukBuroYonetimSistemi.Models.Domain
     {
         [Key]
         public int Id { get; set; }
-        public string YargiTuru { get; set; }
-        public string YargiBirimi { get; set; }
-        public string Mahkeme { get; set; }
-        public string DosyaNo { get; set; }
-        public string GelenEvrakTuru { get; set; }
-        public string TebligTuru { get; set; }
-        public DateTime TebligTarihi { get; set; }
-        public string Diger { get; set; }
-        public string IsTuru { get; set; }
-        public string GelmeTarihi { get; set; }
-        public DateTime HedefSureSonGun { get; set; }
-        public DateTime Gorev { get; set; }
+        public string? YargiTuru { get; set; }
+        public string? YargiBirimi { get; set; }
+        public string? Mahkeme { get; set; }
+        public string? DosyaNo { get; set; }
+        public string? GelenEvrakTuru { get; set; }
+        public string? TebligTuru { get; set; }
+        public DateTime? TebligTarihi { get; set; }
+        public string? Diger { get; set; }
+        public string? IsTuru { get; set; }
+        public string? GelmeTarihi { get; set; }
+        public DateTime? HedefSureSonGun { get; set; }
+        public DateTime? Gorev { get; set; }
 
         // Mahkemenin birçok görevi olabilir
         //public ICollection<GorevAtamalar> GorevAtamalar { get; set; }
         public int GorevAtamalar { get; set; }
         //public GorevAtamalar GorevAtamalar { get; set; }
-        public DateTime DurusmaTarihi { get; set; }
+        //public DateTime DurusmaTarihi { get; set; }
         public DateTime InsertDate { get; set; }
         public DateTime UpdateDate { get; set; }
         public bool IsActive { get; set; }
